faf7ffa73f54470f3d96bbcba9b37bf5d4f25541
ArnyBarnyBears <arnavkumar0407@gmail.com>
2023-08-06 17:57:33 +0100

    fixed search bar functionality

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -21,12 +21,30 @@ function Search() {
       dispatch(getPokemonData(randomPokemonsId));
     }
   }, [allPokemon, dispatch]);
+
+  const getPokemon = async (value: string) => {
+    //for search checks if theres a length in which case checks if the name includes the search term, else displays 20 random pokemon
+    if (value.length) {
+      const pokemons = allPokemon?.filter((pokemon) =>
+        pokemon.name.includes(value.toLowerCase())
+      );
+      dispatch(getPokemonData(pokemons!));
+    } else {
+      const clonedPokemons = [...(allPokemon) as []];
+      const randomPokemonsId = clonedPokemons
+        .sort(() => Math.random() - Math.random())
+        .slice(0, 20);
+      dispatch(getPokemonData(randomPokemonsId));
+    }
+  };
+
   return <>
     <div className="search">
       <input
         type="text"
         className="pokemon-searchbar"
         placeholder="Search Pokemon"
+        onChange={(e) => getPokemon(e.target.value)}
       />
       <PokemonCardGrid pokemons={randomPokemons!} />
     </div>
